4360993c88dec5e4c4d7b21d65a1528b0b219de6
daveaugerinos <dave.augerinos@gmail.com>
2017-02-14 19:56:37 -0800

    Initial build

---
+++ SnakesAndLadders/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SnakesAndLadders
+//
+//  Created by Dave Augerinos on 2017-02-14.
+//  Copyright © 2017 Dave Augerinos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
